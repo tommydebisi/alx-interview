1099c8984202d6030bd2c94e30cbb7b164306a75
tommydebisi <tommydebisi@gmail.com>
2023-01-03 20:01:33 +0100

    :construction: task added

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+    Lockboxes problem solution
+"""
+
+def canUnlockAll(boxes):
+    """
+        Returns a truthy value if all boxes in the array can opened
+    """
+    # check if boxes is a list
+    if type(boxes) != list:
+        return False
+
+    unlocked_set = set()
+    unlocked_set.add(0)
+
+    for index, val in enumerate(boxes):
+        # check if index box can't be unlocked
+        if not (index in unlocked_set):
+            return False
+
+        # add key indexes from unlocked box
+        for elem in val:
+            # go to each elem index and update the set
+            if elem < len(boxes):
+                unlocked_set.update(boxes[elem])
+        unlocked_set.update(val)
+
+    return True
