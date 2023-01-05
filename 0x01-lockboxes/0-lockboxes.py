edabcf3163396d82d67f34b946d5e36b8975630b
tommydebisi <tommydebisi@gmail.com>
2023-01-05 20:11:14 +0100

    :seedling: removed type check

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -8,8 +8,8 @@ def canUnlockAll(boxes):
     """
         Returns a truthy value if all boxes in the array can opened
     """
-    # check if boxes is a list
-    if type(boxes) != list or len(boxes) == 0:
+    # check if boxes is empty
+    if len(boxes) == 0:
         return False
 
     unlocked_set = set()
@@ -17,13 +17,13 @@ def canUnlockAll(boxes):
 
     for index, val in enumerate(boxes):
         # check if index box can't be unlocked
-        if type(val) != list or not (index in unlocked_set):
+        if index not in unlocked_set:
             return False
 
         # add key indexes from unlocked box
         for elem in val:
             # go to each elem index and update the set
-            if elem < len(boxes) and type(elem) == int:
+            if elem < len(boxes):
                 unlocked_set.update(boxes[elem])
         unlocked_set.update(val)
 
