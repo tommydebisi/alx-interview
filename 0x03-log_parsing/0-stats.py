e22d3d64c1fb19025f306db8593ea8a6207d1b1d
tommydebisi <tommydebisi@gmail.com>
2023-01-19 21:10:05 +0100

    :seedling: run the try statement only if main

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -16,32 +16,31 @@ def report_message(file_size, status_obj):
         prints the format message to screen
     """
     print("File size: {}".format(file_size))
-    for key in sorted(status_obj):
-        print("{}: {}".format(key, status_obj.get(key)))
+    for key, val in sorted(status_obj.items()):
+        print("{}: {}".format(key, val))
 
+if __name__ == "__main__":
+    try:
+        for line in sys.stdin:
+            if not re.search(regex, line):
+                continue
 
-try:
-    for line in sys.stdin:
-        if not re.search(regex, line):
-            continue
+            # get status code and file size
+            vals_needed = re.findall(regex, line)
+            fsize_sum += int(vals_needed[0][1])
+            status_code = int(vals_needed[0][0])
 
-        # get status code and file size
-        vals_needed = re.findall(regex, line)
-        fsize_sum += int(vals_needed[0][1])
-        status_code = int(vals_needed[0][0])
+            if not status_obj.get(status_code):
+                status_obj[status_code] = 0
+            status_obj[status_code] += 1
 
-        if not status_obj.get(status_code):
-            status_obj[status_code] = 0
-        status_obj[status_code] += 1
+            line_count += 1
 
-        line_count += 1
+            if line_count == 10:
+                report_message(fsize_sum, status_obj)
+                line_count = 0
 
-        if line_count == 10:
-            report_message(fsize_sum, status_obj)
-            line_count = 0
-
-    if fsize_sum and status_obj:
         report_message(fsize_sum, status_obj)
-except KeyboardInterrupt:
-    report_message(fsize_sum, status_obj)
-    raise
+    except KeyboardInterrupt:
+        report_message(fsize_sum, status_obj)
+        raise
