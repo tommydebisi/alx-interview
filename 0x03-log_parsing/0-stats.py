4796b8a2cf454948aa019b58f4074753085b9575
tommydebisi <tommydebisi@gmail.com>
2023-01-19 18:55:38 +0100

    :construction: log parsing file

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""
+    0-stats mod
+"""
+import sys
+import re
+import signal
+from typing import List
+
+
+def handler(signal_recieved, frame, fsize_sum: int, status_list: List) -> None:
+    """
+        Handles the signal passed in
+    """
+    print("File size: {}".format(fsize_sum))
+    print("\n".join(status_list))
+    raise KeyboardInterrupt
+
+
+line_count = 0
+fsize_sum = 0
+status_obj = dict()
+regex = r'^\d.*\s\-\s\[\d*.*\]\s\"GET.*\"\s(\d*)\s(\d*)$'
+
+for line in sys.stdin:
+    if not re.search(regex, line):
+        line_count += 1
+        continue
+
+    vals_needed = re.findall(regex, line)
+    fsize_sum += int(vals_needed[0][1])
+
+    status_code = int(vals_needed[0][0])
+    if not status_obj.get(status_code):
+        status_obj[status_code] = 0
+    status_obj[status_code] += 1
+
+    line_count += 1
+
+    join_stat = [f"{ky}: {status_obj.get(ky)}" for ky in sorted(status_obj)]
+    signal.signal(signal.SIGINT, lambda signum, frame: handler(signum, frame,
+                                                               fsize_sum,
+                                                               join_stat))
+
+    if line_count == 10:
+        print("File size: {}".format(fsize_sum))
+        for key in sorted(status_obj):
+            print("{}: {}".format(key, status_obj.get(key)))
+        line_count = 0
